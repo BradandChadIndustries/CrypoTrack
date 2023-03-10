d92b024cc33295849c12b20e2175748288af16f0
BradandChadIndustries <brad.chad.industries@gmail.com>
2023-03-10 13:58:59 -0800

    Added API functionality to get Bitcoin Price

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-let value = 0;
-const valueElement = document.getElementById("value");
+const apiKey = '865E580E-3D78-44EA-8763-36960976BFD6';
+const bitcoinSymbol = 'BTC';
+const apiUrl = `https://rest.coinapi.io/v1/exchangerate/${bitcoinSymbol}/USD`;
 
-function incrementValue() {
-	value++;
-	valueElement.innerHTML = value;
-}
+const priceElement = document.getElementById('price');
+
+fetch(apiUrl, {
+  headers: {
+    'X-CoinAPI-Key': apiKey
+  }
+})
+.then(response => response.json())
+.then(data => {
+  const bitcoinPrice = data.rate.toFixed(2);
+  priceElement.innerHTML = `$${bitcoinPrice}`;
+})
+.catch(error => console.error('Error fetching Bitcoin price:', error));
