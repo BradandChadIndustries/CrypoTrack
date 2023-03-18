b5953b681629da63be84e639a68808c01d230917
Aaron Floreani <78060817+AaronFlore@users.noreply.github.com>
2023-03-18 13:57:09 -0700

    Added more cryptos

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
 const apiKey = '865E580E-3D78-44EA-8763-36960976BFD6';
-const bitcoinSymbol = 'BTC';
-const apiUrl = `https://rest.coinapi.io/v1/exchangerate/${bitcoinSymbol}/USD`;
-
+const apiUrl = 'https://rest.coinapi.io/v1/exchangerate';
 const priceElement = document.getElementById('price');
+const nameElement = document.getElementById('crypto-name');
+const selectElement = document.getElementById('crypto-select');
 
-fetch(apiUrl, {
+// Fetch the price for Bitcoin on page load. Bitcoin will be the default displayed crypto.
+fetch(`${apiUrl}/BTC/USD`, {
   headers: {
     'X-CoinAPI-Key': apiKey
   }
 })
-.then(response => response.json())
-.then(data => {
-  const bitcoinPrice = data.rate.toFixed(2);
-  priceElement.innerHTML = `$${bitcoinPrice}`;
-})
-.catch(error => console.error('Error fetching Bitcoin price:', error));
+  .then(response => response.json())
+  .then(data => {
+    const bitcoinPrice = data.rate.toFixed(2);
+    priceElement.innerHTML = `$${bitcoinPrice}`;
+  })
+  .catch(error => console.error('Error fetching cryptocurrency price:', error));
+
+// An event listener for the dropdown menu
+selectElement.addEventListener('change', () => {
+  const selectedCrypto = selectElement.value;
+
+  // Update the cryptocurrency name
+  const selectedOption = selectElement.options[selectElement.selectedIndex];
+  const selectedName = selectedOption.text;
+  nameElement.innerHTML = selectedName;
+
+  // Fetch the price for the selected cryptocurrency
+  const apiUrlWithCrypto = `${apiUrl}/${selectedCrypto}/USD`;
+  fetch(apiUrlWithCrypto, {
+    headers: {
+      'X-CoinAPI-Key': apiKey
+    }
+  })
+    .then(response => response.json())
+    .then(data => {
+      const cryptoPrice = data.rate.toFixed(2);
+      priceElement.innerHTML = `$${cryptoPrice}`;
+    })
+    .catch(error => console.error('Error fetching cryptocurrency price:', error));
+});
